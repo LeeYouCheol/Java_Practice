3805f04a0a75b6acb180125b17f7f06008044e35
Lee You Cheol <seap07@naver.com>
2022-05-03 23:43:38 +0900

    연습 1일차 : 변수 선언2

---
+++ src/day1/P3_Type2.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class P3_Type2 {
+
+	public static void main(String[] args) {
+			//변수를 읽으려면 초기화가 되어있어야 한다.
+			//최기화 되지 않은 변수를 읽어오려고 하면 에러 발생
+		int num; //변수 선언
+		//System.out.println(num); 변수가 초기화 되지 않아 에러 발생
+		
+		num = 10; //변수를 초기화(변수를 변수명에 저장)
+		System.out.println(num);
+
+	}
+
+}
