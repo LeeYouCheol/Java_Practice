ec24790245c268cdd3cfcba90978c8ff0715617f
Lee You Cheol <seap07@naver.com>
2022-05-03 23:46:02 +0900

    연습 2일차 : 부호

---
+++ src/day2/P1_Sign.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class P1_Sign {
+
+	public static void main(String[] args) {
+			//정수형 변수 num1을 서언후 -1로 초기화
+			int num1 = -1;
+			System.out.println(num1);
+			System.out.println(-num1);
+			
+			//정수형 변수 num2를 선언후 2로 초기화
+			int num2 = 2;
+			System.out.println(num2);
+			System.out.println(-num2);
+
+	}
+
+}
