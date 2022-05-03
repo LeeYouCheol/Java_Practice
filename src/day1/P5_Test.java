008bbd35cea44c2aac031971c3d8a128819eafee
Lee You Cheol <seap07@naver.com>
2022-05-03 23:44:30 +0900

    연습 1일차 : 변수 선언 및 초기화 연습문제

---
+++ src/day1/P5_Test.java
@@ -0,0 +1,36 @@
+package day1;
+
+public class P5_Test {
+
+	public static void main(String[] args) {
+			/* 과일가게에서 과일 판매량을 기록하려한다.
+			 * 사과, 오렌지 판매량을 기록하기 위한 변수를 선언하세요
+			 * */
+			int apple = 0;
+			int orange = 0;
+			
+			/* 학생 한명의 국어, 영어, 수학 성적을 저장하려 한다.
+			 * 이때 필요한 변수를 선언하세요.
+			 * 성적은 정수로 관리(조건1)
+			 * 성적은 실수로 관리(조건2)
+			 * */
+			//조건1
+			int kscore1 = 0;
+			int escore1 = 0;
+			int mscore1 = 0;
+			//조건2
+			double kscore2 = 0;
+			double escore2 = 0;
+			double mscore2 = 0;
+			
+			/* 대학생의 학점을 출력하고 한다.
+			 * 이때 학점을 저장하는 변수를 선언하세요.
+			 * 학점은 0~4.5로 관리(조건1)
+			 * 학점은 F~A로 관리(조건2)
+			 * */
+			//조건1
+			double score1 = 0.0;
+			char score2 = 'f';
+	}
+
+}
