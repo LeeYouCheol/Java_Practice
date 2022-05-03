db3abc03546bd52c846573023d7623e893411b78
Lee You Cheol <seap07@naver.com>
2022-05-03 23:43:14 +0900

    연습 1일차 : 문자열 출력

---
+++ src/day1/P1_Hello.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class P1_Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");// println은 다음번 출력하는 값을 한줄 아래에 출력
+		System.out.print("Hello!"); // print는 다음번 출력하는 값을 이어서 출력
+		System.out.print("Hello");
+	}
+
+}
