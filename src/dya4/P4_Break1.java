efcb8e1f1769bfe8e1e5015f53986011ed982553
Lee You Cheol <seap07@naver.com>
2022-05-09 10:16:47 +0900

    연습 4일차 : break

---
+++ src/dya4/P4_Break1.java
@@ -0,0 +1,16 @@
+package dya4;
+
+public class P4_Break1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("Hello");
+			if(i == 3) {
+				break;
+			}
+		}
+
+	}
+
+}
