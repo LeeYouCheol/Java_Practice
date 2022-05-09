85fbedab3adadbcd240480953121b9fae61be6c7
Lee You Cheol <seap07@naver.com>
2022-05-09 11:06:34 +0900

    연습 7일차 : 배열 복사

---
+++ src/day7/P4_Arry4_Copy.java
@@ -0,0 +1,32 @@
+package day7;
+
+public class P4_Arry4_Copy {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = num1;
+		System.out.println("num1 : " + num1 + ", num2 : " + num2);
+		/* arr2 배열을 생성하여 arr1배열을 복사하고, arr2배열값을 출력하세요.
+		 * */
+		int arr1[] = {1,4,6,8};
+		int arr2[] = new int[arr1.length];
+		int arr3[] = arr1;
+		int arr4[] = new int[arr1.length];
+		
+		//arr1의 값들을 반복문을 통해 arr2에 복사
+		for(int i = 0; i< arr1.length; i++) {
+			arr2[i] = arr1[i];
+		}
+		//arr1의 값들을 기능을 통해 arr4에 복사. p.168
+				//arr1배열의 있는 값들을 0번지부터 arr.length개 복사하여
+				//arr4배열의 0번지부터 붙여넣어줌
+		System.arraycopy(arr1, 0, arr4, 0, arr1.length-2);
+		arr1[0] = 0;
+		
+		System.out.print("arr1 : ");
+		for(int i = 0; i < arr1.length; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+	}
+
+}
